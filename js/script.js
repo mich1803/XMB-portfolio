a847f88829c96ba72116bd1b2a53d882a95ec141
Michele Magrini <michelemagrini2003@gmail.com>
2026-03-09 20:04:06 +0100

    Adjust first-section positioning and left-anchor submenu text layout

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const playNavSound = () => {
 const moveMenu = (index) => {
   const width = document.body.clientWidth;
   const offsets = [
-    ['-28%', '8%', '8%'],
+    ['-38%', '-24%', '-24%'],
     ['-10%', '18%', '18%'],
     ['22%', '32%', '39%'],
     ['50%', '47%', '60%'],
