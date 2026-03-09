aee8b63b8ff87e01c45c5a6eb7fd0185455c6fca
Michele Magrini <michelemagrini2003@gmail.com>
2026-03-09 19:56:57 +0100

    Restore title glow and normalize submenu alignment under section icons

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const playNavSound = () => {
 const moveMenu = (index) => {
   const width = document.body.clientWidth;
   const offsets = [
-    ['-40%', '0', '0'],
+    ['-28%', '8%', '8%'],
     ['-10%', '18%', '18%'],
     ['22%', '32%', '39%'],
     ['50%', '47%', '60%'],
